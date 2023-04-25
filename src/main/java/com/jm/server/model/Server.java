8fa332b0e48c28ed1b92331e43a93a55c49ecd0e
James Muralles <james.muralles23@gmail.com>
2023-04-25 12:58:46 -0500

    added model and enumeration files

---
+++ src/main/java/com/jm/server/model/Server.java
@@ -0,0 +1,14 @@
+package com.jm.server.model;
+
+import com.jm.server.enumeration.Status;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+
+}
