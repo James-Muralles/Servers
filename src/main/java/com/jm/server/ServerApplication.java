bdc0cc2ff54e4de2d80ef17fce2b5d92007d3e18
James Muralles <james.muralles23@gmail.com>
2023-04-25 11:42:57 -0500

    test

---
+++ src/main/java/com/jm/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.jm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
