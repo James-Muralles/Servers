6e2ccb1539724132fe507a44c255a7e4ea501f0b
James Muralles <james.muralles23@gmail.com>
2023-04-25 15:35:17 -0500

    created server repo interface

--- src/main/java/com/jm/server/enumeration/Status.java
+++ src/main/java/com/jm/server/enumeration/Status.java
@@ -1,16 +1,5 @@
 package com.jm.server.enumeration;
 
-import javax.persistence.Entity;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-
 public enum Status {
     SERVER_UP("SERVER_UP"),
     SERVER_DOWN("SERVER_DOWN");
--- src/main/java/com/jm/server/model/Server.java
+++ src/main/java/com/jm/server/model/Server.java
@@ -2,8 +2,28 @@ package com.jm.server.model;
 
 import com.jm.server.enumeration.Status;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Server {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty or null")
     private String ipAddress;
     private String name;
     private String memory;
