625582dddae2a5d14eec3d0c742fc3dd6abb3969
James Muralles <james.muralles23@gmail.com>
2023-04-25 12:59:39 -0500

    updated status

---
+++ src/main/java/com/jm/server/enumeration/Status.java
@@ -0,0 +1,27 @@
+package com.jm.server.enumeration;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public enum Status {
+    SERVER_UP("SERVER_UP"),
+    SERVER_DOWN("SERVER_DOWN");
+
+    private final String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+}
